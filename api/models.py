e62256024516b8d09b3fdb94c912f221c2318978
unknown <01shubhamupadhyay@gmail.com>
2024-02-05 02:10:51 +0530

    Company Model Created

--- api/models.py
+++ api/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
-#Creating company models
+# Creating company models
 
-
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    about = models.TextField()
+    type = models.CharField(max_length=50,choices=(('IT','IT'),
+                                                   ('Non IT','Non IT'),
+                                                   ('Mobile Phone','Mobile Phones')))
+    added_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
