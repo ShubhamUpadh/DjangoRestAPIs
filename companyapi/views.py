412a4113c58cfcdcf576f1dfd43dd2f70a98bb21
unknown <01shubhamupadhyay@gmail.com>
2024-02-05 01:42:25 +0530

    JSON Example

--- companyapi/views.py
+++ companyapi/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def home_page(request):
+    friends = ['abc','def','efg']
     strs = '''<h1>Home Page</h1>
             <br>
             <p>This page has been created using django</p>'''
     print("Home Page Requested")
-    return HttpResponse(strs)
+    return JsonResponse(friends,safe=False)
